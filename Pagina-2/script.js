257688512ca9ef744debd9de888aaf0888226170
System Broken <42917403+sysbroken@users.noreply.github.com>
2022-12-13 16:32:13 -0500

    Pagina-2

--- Pagina-2/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function(){
-    var menu = document.querySelector(".bar-principal");
-    menu.classList.toggle("down",window.scrollY>0);
-    menu.classList.toggle("black",window.scrollY>700);
-})
